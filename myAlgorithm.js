590a1a8d6c463b6227cf3bf6bc7b9b4f234217fd
AlvaroPaniaguaT <alvaritoa7x@gmail.com>
2018-08-15 16:07:34 +0200

    Added code to resolve follow-line (not completed)

--- myAlgorithm.js
+++ myAlgorithm.js
@@ -4,12 +4,37 @@
         // Always use this code above.
         $("#scene").on('loaded', function() {
             var myRobot = new RobotI('a-pibot');
-            myRobot.setV(-0.2);
-            myRobot.setW(-0.4);
+            myRobot.setV(0);
+            //myRobot.setW(-);
             
             
             setInterval(()=>{
                 var img = myRobot.getImage();
-                cv.imshow('outputCanvas', img);
+        	    let grayImg = new cv.Mat();
+		        let dst2 = new cv.Mat();
+
+		        cv.cvtColor(img, grayImg, cv.COLOR_RGBA2GRAY);
+		        
+                let ksize = new cv.Size(9,9);
+		        let grayBlurImg = new cv.Mat();
+		        cv.GaussianBlur(grayImg, grayBlurImg, ksize, 0, 0, cv.BORDER_DEFAULT);
+		        
+		        let threshImg = new cv.Mat();
+		        cv.threshold(grayBlurImg , threshImg, 201 ,255, cv.THRESH_BINARY);
+		        
+		        let contours = new cv.MatVector();
+		        let hierarchy = new cv.Mat();
+		        cv.findContours(threshImg, contours, hierarchy, cv.RETR_CCOMP, cv.CHAIN_APPROX_SIMPLE);
+                
+                let aux = new cv.Mat.zeros(img.cols, img.rows, cv.CV_8UC3);
+		        if(contours.size() > 0){
+		            for(var i = 0; i < contours.size(); i++){
+		                let color = new cv.Scalar(Math.round(Math.random()*255), Math.round(Math.random()*255), Math.round(Math.random()*255));
+		                cv.drawContours(aux, contours, i, color, 1, cv.LINE_8, hierarchy, 100);
+		            }
+		        }
+                //cv.imshow('outputCanvas', aux);
+                
             }, 33);
         });
+      
