7044f586f534ac85033600e34ae2781f40591de6
AlvaroPaniaguaT <alvaritoa7x@gmail.com>
2018-08-22 18:44:32 +0200

    Added custom event emitter to catch raycaster collides.

--- js/followBodyComponent.js
+++ js/followBodyComponent.js
@@ -14,5 +14,10 @@ AFRAME.registerComponent("follow-body", {
 
     this.el.setAttribute("position", pibotPos);
     this.el.setAttribute("rotation", this.pibot.getAttribute("rotation"));
+    this.el.addEventListener('raycaster-intersection', function(evt){
+      console.log("collideeee")
+      var e = new CustomEvent('intersection-detected', {detail: evt.detail});
+      this.dispatchEvent(e);
+    });
   }
 });
